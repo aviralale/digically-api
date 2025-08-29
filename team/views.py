63e3f77ad93d0ebd986ff4957576c055009f07ea
Aviral Ale <69aviral@gmail.com>
2025-08-29 16:35:24 +0545

    fixed access by any

--- team/views.py
+++ team/views.py
@@ -1,8 +1,10 @@
 from .models import TeamMember
 from .serializers import TeamSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 
 class TeamMemberViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = TeamMember.objects.all()
     serializer_class = TeamSerializer
+    permission_classes = [AllowAny]
